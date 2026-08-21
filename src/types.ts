c0d6520e782ae0a5d962e1b595e09e87abc5e7ce
Chaiball <chaiball0511@gmail.com>
2026-08-21 23:25:58 +0800

    chore: add src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export type Phase =
+  | 'START'
+  | 'DAWN_PHASE'
+  | 'WITCH_BLACK_CAT_ACTION'
+  | 'DAY_START'
+  | 'NIGHT_PHASE'
+  | 'WITCH_KILL_ACTION'
+  | 'SHERIFF_PROTECT_ACTION'
+  | 'RESULTS_READY'
+  | 'RESULTS_REVEALED'
+
+export interface Player {
+  id: number
+  name: string
+}
+
+export interface GameState {
+  players: Player[]
+  phase: Phase
+  nightNumber: number
+  blackCatTarget: number | null
+  witchKillTarget: number | null
+  sheriffProtectTarget: number | null
+}
+
+export interface VoiceSettings {
+  enabled: boolean
+  rate: number
+}
+
